e2ab1ffb87a6eb2a1c19e06ec9e8a3d20b651450
Septdir <septdir@gmail.com>
2017-10-23 18:41:32 +0300

    Top Panel

--- html/layouts/template/header.php
+++ html/layouts/template/header.php
@@ -10,28 +10,31 @@
 
 defined('_JEXEC') or die;
 
-use Joomla\CMS\Uri\Uri;
 
 $header = $displayData;
 
 ?>
-<header class="tm-top uk-card-default uk-box-shadow-small" data-uk-sticky="">
-	<div class="uk-container uk-height-1-1">
-		<div class="uk-grid-large uk-grid-match uk-height-1-1" data-uk-grid="">
-			<div class="uk-width-1-5@m uk-flex uk-flex-middle uk-flex-left@m">
-				<div>
-					<a class="menu uk-margin-small-right" data-uk-icon="icon: menu; ratio: 1.5" href="#navigation">
-					</a>
-					<a class="logo" href="<?php echo Uri::root(); ?>">
-						<?php echo $header->logo->element; ?>
-					</a>
+<header class="tm-top">
+	<div class="tm-toppanel uk-card-default uk-box-shadow-small" data-uk-sticky="">
+		<div class="uk-container uk-height-1-1">
+			<div class="uk-grid-large uk-grid-match uk-height-1-1" data-uk-grid="">
+				<div class="left uk-width-1-5@m uk-flex uk-flex-middle uk-flex-left@m">
+					<div>
+						<a class="menu uk-margin-small-right" data-uk-icon="icon: menu; ratio: 1.5" href="#navigation">
+						</a>
+						<a class="logo" href="<?php echo \Joomla\CMS\Uri\Uri::root(); ?>">
+							<?php echo $header->logo->element; ?>
+						</a>
+					</div>
 				</div>
-			</div>
-			<div class="uk-width-expand@m uk-flex uk-flex-middle uk-flex-center@m">
-				middle
-			</div>
-			<div class="uk-width-1-5@m uk-flex uk-flex-middle uk-flex-right@m">
-				right
+				<div class="center uk-width-expand@m uk-flex uk-flex-top uk-flex-center@m">
+					<ul>
+						<jdoc:include type="modules" name="toppanel-center" style="toppanel_center"/>
+					</ul>
+				</div>
+				<div class="right uk-width-1-5@m uk-flex uk-flex-middle uk-flex-right@m">
+					<jdoc:include type="modules" name="toppanel-right" style="toppanel_right"/>
+				</div>
 			</div>
 		</div>
 	</div>
--- html/modules.php
+++ html/modules.php
@@ -10,10 +10,32 @@
 
 defined('_JEXEC') or die;
 
+use Joomla\Registry\Registry;
+
 function modChrome_no($module, &$params, &$attribs)
 {
 	if ($module->content)
 	{
 		echo $module->content;
+	}
+}
+
+function modChrome_toppanel_center($module, &$params, &$attribs)
+{
+	if ($module->content)
+	{
+		$params = new Registry($params);
+		$class  = 'item-' . $module->id;
+		$class  .= ' level-1';
+		$class  .= ' ' . $params->get('moduleclass_sfx');
+		$class  .= ' uk-height-1-1';
+
+		echo '<li class="' . $class . '">';
+		echo '<a class="uk-flex uk-flex-center uk-flex-middle uk-height-1-1">' . $module->title
+			. '<i data-uk-icon="icon: chevron-down; ratio: 0.8" class="uk-margin-small-left"></i>' . '</a>';
+		echo '<div data-uk-dropdown="mode: click">' . $module->content . '</div>';
+		echo '</li>';
+		//echo '<pre>', print_r($module, true), '</pre>';
+		//echo ;
 	}
 }
