620014012299ec27d1817add0e799865e2104af1
Septdir <septdir@gmail.com>
2017-11-20 13:25:18 +0300

    Change version to 4.9.2

--- js/template.js
+++ js/template.js
@@ -1,6 +1,6 @@
 /*
  * @package    Nerudas Template
- * @version    4.9.1
+ * @version    4.9.2
  * @author     Nerudas  - nerudas.ru
  * @copyright  Copyright (c) 2013 - 2017 Nerudas. All rights reserved.
  * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
