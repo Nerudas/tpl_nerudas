9ee855b8ac82199b17dc8926245be5383c551e96
Septdir <septdir@gmail.com>
2017-11-24 13:50:58 +0300

    Field layout override

---
+++ html/layouts/joomla/form/renderfield.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package    Nerudas Template
+ * @version    4.9.3
+ * @author     Nerudas  - nerudas.ru
+ * @copyright  Copyright (c) 2013 - 2017 Nerudas. All rights reserved.
+ * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
+ * @link       https://nerudas.ru
+ */
+
+defined('_JEXEC') or die;
+
+extract($displayData);
+
+if (!empty($options['showonEnabled']))
+{
+	JHtml::_('jquery.framework');
+	JHtml::_('script', 'jui/cms.js', array('version' => 'auto', 'relative' => true));
+}
+
+$class = empty($options['class']) ? '' : ' ' . $options['class'];
+$rel   = empty($options['rel']) ? '' : ' ' . $options['rel'];
+?>
+<div class="uk-form-row <?php echo $class; ?>"<?php echo $rel; ?>>
+	<?php if (empty($options['hiddenLabel'])) : ?>
+		<?php echo $label; ?>
+	<?php endif; ?>
+	<div class="uk-form-controls"><?php echo $input; ?></div>
+</div>
