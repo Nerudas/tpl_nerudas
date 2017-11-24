1c33a9f759b78a4640a66379895bd915ac66fe52
Septdir <septdir@gmail.com>
2017-11-24 17:54:59 +0300

    Add mod_remozona_request override

--- html/mod_remzona_request/default.php
+++ html/mod_remzona_request/default.php
@@ -10,7 +10,6 @@
 
 defined('_JEXEC') or die;
 
-use Joomla\CMS\Router\Route;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\HTML\HTMLHelper;
 
@@ -18,29 +17,40 @@ use Joomla\CMS\HTML\HTMLHelper;
 
 <div data-mod-remozona-request>
 	<div class="result">
-		<div class="error">
+		<div class="error uk-alert uk-alert-danger">
 
 		</div>
-		<div class="success">
+		<div class="success uk-alert uk-alert-success">
 		</div>
 	</div>
-	<form class="form-validate" enctype="multipart/form-data">
-		<?php echo $form->renderField('text'); ?>
-		<?php echo $form->renderField('phones'); ?>
-		<?php echo $form->renderField('region'); ?>
-		<div class="control-group">
-			<div class="control-label">
-				<?php echo Text::_('COM_REMZONA_REQUEST_CAR'); ?>
+	<form class="form-validate uk-form uk-form-stacked" enctype="multipart/form-data">
+		<div class="uk-form-row">
+			<div class="uk-form-controls">
+				<?php echo $form->getInput('text'); ?>
 			</div>
-			<div class="controls">
+		</div>
+		<div class="uk-form-row">
+			<div class="uk-form-controls">
 				<?php echo $form->getInput('brand'); ?>
 				<?php echo $form->getInput('model'); ?>
 				<?php echo $form->getInput('year'); ?>
 			</div>
 		</div>
-		<?php echo $form->renderField('suppliertypes'); ?>
-		<div class="control-group">
-			<a class="submit"><?php echo Text::_('JSUBMIT'); ?></a>
+		<div class="uk-form-row">
+			<div class="uk-form-controls">
+				<?php echo $form->getInput('region'); ?>
+				<?php echo $form->getInput('suppliertypes'); ?>
+			</div>
+		</div>
+		<div class="uk-form-row uk-margin-bottom-remove">
+			<div class="uk-form-controls uk-margin-bottom-remove uk-clearfix">
+				<div class="uk-align-medium-left uk-margin-small-bottom">
+				<?php echo $form->getInput('phones'); ?>
+				</div>
+				<div class="uk-align-medium-right uk-margin-small-bottom">
+					<a class="submit uk-button uk-button-success"><?php echo Text::_('JSUBMIT'); ?></a>
+				</div>
+			</div>
 		</div>
 		<input type="hidden" name="return" value="<?php echo (string) JUri::getInstance(); ?>"/>
 		<?php echo HTMLHelper::_('form.token'); ?>
