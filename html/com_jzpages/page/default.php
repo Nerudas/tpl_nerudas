3eb92fb4e1ac5e37ddadbe97921cc8ae57179a06
Septdir <septdir@gmail.com>
2017-12-28 14:30:11 +0300

    Add jzpages

---
+++ html/com_jzpages/page/default.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @package    Nerudas Template
+ * @version    4.9.4
+ * @author     Nerudas  - nerudas.ru
+ * @copyright  Copyright (c) 2013 - 2017 Nerudas. All rights reserved.
+ * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
+ * @link       https://nerudas.ru
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Layout\LayoutHelper;
+
+?>
+<div id="pages">
+	<?php echo LayoutHelper::render('template.title', array()); ?>
+	<?php if (!empty($this->item->content)): ?>
+		<div>
+			<?php echo $this->item->content; ?>
+		</div>
+	<?php endif; ?>
+</div>
+
