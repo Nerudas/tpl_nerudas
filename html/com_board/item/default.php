772e9cde43740d60dfae78d342804cbc4846929f
Septdir <septdir@gmail.com>
2018-01-16 18:21:38 +0300

    Add default item

---
+++ html/com_board/item/default.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @package    Nerudas Template
+ * @version    4.9.5
+ * @author     Nerudas  - nerudas.ru
+ * @copyright  Copyright (c) 2013 - 2018 Nerudas. All rights reserved.
+ * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
+ * @link       https://nerudas.ru
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Layout\LayoutHelper;
+use Joomla\CMS\Factory;
+
+HTMLHelper::_('jquery.framework');
+
+HTMLHelper::_('script', '//api-maps.yandex.ru/2.1/?lang=ru-RU', array('version' => 'auto', 'relative' => true));
+HTMLHelper::_('script', 'modalmap.min.js', array('version' => 'auto', 'relative' => true));
+
+
+?>
+
+<?php echo LayoutHelper::render('template.title', array('edit' => $this->editLink)); ?>
+<?php echo '<pre>', print_r($this->item, true), '</pre>';
