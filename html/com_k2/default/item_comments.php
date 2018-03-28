e50720f0b6b5ec489d070e8dbf56b6637bfdaebb
Septdir <septdir@gmail.com>
2018-03-29 00:48:11 +0300

    fix ola comments author

--- html/com_k2/default/item_comments.php
+++ html/com_k2/default/item_comments.php
@@ -9,36 +9,31 @@
  */
 
 defined('_JEXEC') or die;
+
+use Joomla\CMS\Router\Route;
+
 $app = JFactory::getApplication();
 $doc = JFactory::getDocument();
+
+JLoader::register('ProfilesHelperRoute', JPATH_SITE . '/components/com_profiles/helpers/route.php');
 ?>
 <?php if (isset($this->item->comments)) : ?>
 	<div id="comments" class="itemlist">
 		<?php echo $this->loadTemplate('comments_form'); ?>
 		<?php foreach ($this->item->comments as $key => $comment):
-			$comment->author = NerudasProfilesHelper::getProfile($comment->userID);
+			$comment->authorLink = Route::_(ProfilesHelperRoute::getProfileRoute($comment->userID));
 			?>
 			<div id="comment-<?php echo $comment->id; ?>" class="item">
 				<div class="uk-grid uk-grid-small">
 
 					<div class="uk-width-1-1 uk-width-small-1-1 uk-width-medium-1-10 uk-hidden-small">
-						<a class="uk-text-middle uk-avatar-48 <?php echo $comment->author->status; ?>"
-						   href="<?php echo $comment->author->link; ?>"
-						   style="background-image: url('<?php echo $comment->author->avatar->small; ?>')">
-						</a>
+
 					</div>
 					<div class="uk-width-1-1 uk-width-small-1-1 uk-width-medium-9-10">
 						<div class="header">
-							<a class="name uk-link-muted" href="<?php echo $comment->author->link; ?>" target="_blank">
-								<?php echo $comment->author->name; ?>
+							<a class="name uk-link-muted" href="<?php echo $comment->authorLink; ?>" target="_blank">
+								<?php echo $comment->userName; ?>
 							</a>
-							<?php if ($comment->author->job): ?>
-								<a class="job uk-text-muted" href="<?php echo $comment->author->job->link; ?>"
-								   target="_blank">
-									<?php echo $comment->author->job->name; ?>
-								</a>
-
-							<?php endif; ?>
 							<span class="date uk-text-muted uk-float-right">
 						<?php echo JHTML::_('date', $comment->commentDate, 'd M H:i'); ?>
 					</span>
