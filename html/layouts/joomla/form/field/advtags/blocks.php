72f7ea419f2402e02cdfe12055d36d9f37ec2c70
Septdir <septdir@gmail.com>
2018-04-11 14:07:12 +0300

    add only title & remove js

--- html/layouts/joomla/form/field/advtags/blocks.php
+++ html/layouts/joomla/form/field/advtags/blocks.php
@@ -10,7 +10,6 @@
 
 defined('_JEXEC') or die;
 
-use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Layout\LayoutHelper;
 
 extract($displayData);
@@ -27,9 +26,6 @@ extract($displayData);
  * @var   string $class    Classes for the input.
  */
 
-HTMLHelper::_('jquery.framework');
-//HTMLHelper::_('stylesheet', 'media/plg_fieldtypes_advtags/field.min.css', array('version' => 'auto'));
-HTMLHelper::_('script', 'media/plg_fieldtypes_advtags/field.min.js', array('version' => 'auto'));
 ?>
 
 <div id="<?php echo $id; ?>" data-input-advtags="blocks" class="<?php echo $class; ?>">
@@ -38,10 +34,12 @@ HTMLHelper::_('script', 'media/plg_fieldtypes_advtags/field.min.js', array('vers
 		<?php foreach ($root as $option): ?>
 			<li class="item option-<?php echo $option->key; ?> level-<?php echo $option->level; ?> uk-width-medium-1-2">
 				<label for="<?php echo $option->id; ?>" class=" uk-h4 uk-margin-small-bottom uk-display-block">
+					<?php if (!$option->only_title): ?>
+						<input type="checkbox" name="<?php echo $option->name; ?>" id="<?php echo $option->id; ?>"
+							   value="<?php echo $option->value; ?>"
+							   data-parent="<?php echo $option->parent; ?>" <?php echo $option->checked; ?>>
+					<?php endif; ?>
 					<strong><?php echo $option->text; ?></strong>
-					<input type="checkbox" name="<?php echo $option->name; ?>" id="<?php echo $option->id; ?>"
-						   value="<?php echo $option->value; ?>" class="uk-hidden"
-						   data-parent="<?php echo $option->parent; ?>" <?php echo $option->checked; ?>>
 				</label>
 				<?php if (!empty($children[$option->key]))
 				{
