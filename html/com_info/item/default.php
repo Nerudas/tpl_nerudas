b47cd13191a41eca13f65e00d029bf7519ed4267
Septdir <septdir@gmail.com>
2018-05-10 14:25:26 +0300

    Add info item override

---
+++ html/com_info/item/default.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package    Nerudas Template
+ * @version    4.9.9
+ * @author     Nerudas  - nerudas.ru
+ * @copyright  Copyright (c) 2013 - 2018 Nerudas. All rights reserved.
+ * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
+ * @link       https://nerudas.ru
+ */
+
+defined('_JEXEC') or die;
+
+echo '<pre>', print_r($this->item, true), '</pre>';
+
+echo '<pre>', print_r($this->related, true), '</pre>';
