36c457fb71d35d1c5ee2358faa61092e1cf1623a
Septdir <septdir@gmail.com>
2018-05-29 15:18:39 +0300

    Change info default item list layout

--- html/layouts/components/com_info/list/item/default.php
+++ html/layouts/components/com_info/list/item/default.php
@@ -38,7 +38,6 @@ $item = $displayData;
 				<?php endif; ?>
 			</div>
 			<div class="text uk-margin-small-bottom"><?php echo $item->introtext; ?></div>
-
 			<?php if (!empty($item->tags->itemTags)): ?>
 				<div class="tags uk-margin-small-bottom">
 					<?php foreach ($item->tags->itemTags as $tag): ?>
@@ -48,26 +47,27 @@ $item = $displayData;
 					<?php endforeach; ?>
 				</div>
 			<?php endif; ?>
-
-			<div class="meta-info uk-flex uk-flex-space-between">
-				<div class="uk-text-right">
-					<div class="uk-text-nowrap">
-						<time class="timeago uk-text-muted uk-text-small uk-text-nowrap uk-margin-small-left"
-							  data-uk-tooltip
-							  datetime="<?php echo HTMLHelper::date($item->created, 'c'); ?>"
-							  title="<?php echo HTMLHelper::date($item->created, 'd.m.Y H:i'); ?>"></time>
-					</div>
+		</div>
+	</div>
+	<div class="uk-margin-small-top">
+		<div class="meta-info uk-flex uk-flex-space-between">
+			<div class="uk-text-right">
+				<div class="uk-text-nowrap">
+					<time class="timeago uk-text-muted uk-text-small uk-text-nowrap uk-margin-small-left"
+						  data-uk-tooltip
+						  datetime="<?php echo HTMLHelper::date($item->created, 'c'); ?>"
+						  title="<?php echo HTMLHelper::date($item->created, 'd.m.Y H:i'); ?>"></time>
 				</div>
-				<div class="uk-text-left uk-margin-small-bottom uk-text-nowrap">
-					<a href="<?php echo $item->link; ?>"
-					   class="uk-badge uk-badge-white uk-margin-small-left">
-						<i class="uk-icon-eye uk-margin-small-right"></i><?php echo $item->hits; ?>
-					</a>
-					<a href="<?php echo $item->link; ?>#comments"
-					   class="uk-badge uk-badge-white uk-margin-small-left">
-						<i class="uk-icon-comment-o uk-margin-small-right"></i>0
-					</a>
-				</div>
+			</div>
+			<div class="uk-text-left uk-margin-small-bottom uk-text-nowrap">
+				<a href="<?php echo $item->link; ?>"
+				   class="uk-badge uk-badge-white uk-margin-small-left">
+					<i class="uk-icon-eye uk-margin-small-right"></i><?php echo $item->hits; ?>
+				</a>
+				<a href="<?php echo $item->link; ?>#comments"
+				   class="uk-badge uk-badge-white uk-margin-small-left">
+					<i class="uk-icon-comment-o uk-margin-small-right"></i>0
+				</a>
 			</div>
 		</div>
 	</div>
