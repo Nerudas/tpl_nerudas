ce913e4815f5fbb42b9f27b58a983f463a17cc84
Septdir <septdir@gmail.com>
2018-05-31 11:44:48 +0300

    Add com_discussions overrides

---
+++ html/com_discussions/topic/default.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @package    Nerudas Template
+ * @version    4.9.13
+ * @author     Nerudas  - nerudas.ru
+ * @copyright  Copyright (c) 2013 - 2018 Nerudas. All rights reserved.
+ * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
+ * @link       https://nerudas.ru
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Layout\LayoutHelper;
+
+?>
+
+<h1><?php echo Text::_('COM_DISCUSSIONS_TOPIC'); ?></h1>
+<?php //echo '<pre>', print_r($this->topic, true), '</pre>'; ?>
+<?php
+$data               = array();
+$data['items']      = $this->items;
+$data['total']      = $this->total;
+$data['pagination'] = $this->pagination;
+$data['addForm']    = $this->addPostForm;
+echo LayoutHelper::render('components.com_discussions.posts.list', $data); ?>
---
+++ html/com_discussions/topics/default.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @package    Nerudas Template
+ * @version    4.9.13
+ * @author     Nerudas  - nerudas.ru
+ * @copyright  Copyright (c) 2013 - 2018 Nerudas. All rights reserved.
+ * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
+ * @link       https://nerudas.ru
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Factory;
+
+HTMLHelper::_('formbehavior.chosen', 'select');
+
+$filters = array_keys($this->filterForm->getGroup('filter'));
+?>
+<?php echo '<pre>', print_r($this->category, true), '</pre>'; ?>
+<form action="<?php echo htmlspecialchars(Factory::getURI()->toString()); ?>" method="get" name="adminForm">
+	<?php foreach ($filters as $filter): ?>
+		<?php echo $this->filterForm->renderField(str_replace('filter_', '', $filter), 'filter'); ?>
+	<?php endforeach; ?>
+	<button type="submit"><?php echo Text::_('JSEARCH_FILTER_SUBMIT'); ?></button>
+	<a href="<?php echo $this->category->link; ?>"><?php echo Text::_('JCLEAR'); ?></a>
+</form>
+<?php if (!empty($this->items)): ?>
+	<?php foreach ($this->items as $item): ?>
+		<div>
+			<div>
+				<a href="<?php echo $item->link; ?>"><?php echo $item->title; ?></a>
+			</div>
+			<?php echo '<pre>', print_r($item, true), '</pre>'; ?>
+		</div>
+
+	<?php endforeach; ?>
+<?php endif; ?>
+
+
+<?php echo $this->pagination->getListFooter(); ?>
