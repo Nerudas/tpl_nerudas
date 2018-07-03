91d81054f92a0df457339d79f4f720c6d0cf986f
Septdir <septdir@gmail.com>
2018-07-03 16:17:35 +0300

    Change board

--- html/com_board/list/default.php
+++ html/com_board/list/default.php
@@ -17,8 +17,6 @@ use Joomla\CMS\Factory;
 
 
 require_once JPATH_THEMES . '/nerudas/helper.php';
-$showAdvansedFiler = tplNerudasHelper::checkAdvansedFilterActivity($this->filterForm,
-	array('for_when', 'price', 'payment_method', 'prepayment', 'allregion', 'onlymy'));
 
 HTMLHelper::_('jquery.framework');
 HTMLHelper::_('formbehavior.chosen', 'select');
@@ -28,8 +26,7 @@ HTMLHelper::_('script', 'modalmap.min.js', array('version' => 'auto', 'relative'
 ?>
 
 <div id="board" class="itemlist">
-	<?php $layouts = array('list' => $this->listLink, 'map' => $this->mapLink, 'active' => 'list');
-	echo LayoutHelper::render('template.title', array('add' => $this->addLink, 'layouts' => $layouts)); ?>
+	<?php echo LayoutHelper::render('template.title', array('add' => $this->addLink)); ?>
 	<div class="uk-panel uk-panel-box uk-margin-bottom uk-panel-box-secondary">
 		<form action="<?php echo htmlspecialchars(Factory::getURI()->toString()); ?>" method="get" name="adminForm"
 			  class="uk-form filter">
@@ -46,8 +43,7 @@ HTMLHelper::_('script', 'modalmap.min.js', array('version' => 'auto', 'relative'
 						$class = $this->filterForm->getFieldAttribute('search', 'class', '', 'filter') . ' uk-width-1-1';
 						$this->filterForm->setFieldAttribute('search', 'class', $class, 'filter');
 						echo $this->filterForm->getInput('search', 'filter'); ?>
-						<div class="uk-button-group left-input advanced-fiter
-							<?php echo ($showAdvansedFiler) ? 'uk-hidden' : ''; ?>">
+						<div class="uk-button-group left-input advanced-fiter">
 							<a href="<?php echo $this->link; ?>"
 							   class="uk-button uk-text-danger uk-icon-times">
 							</a>
@@ -58,67 +54,11 @@ HTMLHelper::_('script', 'modalmap.min.js', array('version' => 'auto', 'relative'
 					</div>
 				</div>
 			</div>
-			<div class="uk-form-row advanced-fiter uk-text-right <?php echo ($showAdvansedFiler) ? 'uk-hidden' : ''; ?>">
-				<a data-uk-toggle="{target:'.advanced-fiter'}">
-					<?php echo Text::_('TPL_NERUDAS_ADVANCED_FITER'); ?>
-				</a>
-			</div>
-			<div class="uk-form-row advanced-fiter <?php echo (!$showAdvansedFiler) ? ' uk-hidden' : ''; ?>">
-				<?php echo $this->filterForm->getInput('price', 'filter'); ?>
-			</div>
-			<div class="advanced-fiter <?php echo (!$showAdvansedFiler) ? ' uk-hidden' : ''; ?>">
-				<div class="uk-grid uk-grid-small" data-uk-margin>
-					<div class="uk-width-medium-1-3">
-						<?php echo $this->filterForm->renderField('payment_method', 'filter'); ?>
-					</div>
-					<div class="uk-width-medium-1-3">
-						<?php echo $this->filterForm->renderField('prepayment', 'filter'); ?>
-					</div>
-					<div class="uk-width-medium-1-3">
-						<?php echo $this->filterForm->renderField('for_when', 'filter'); ?>
-					</div>
-				</div>
-			</div>
-			<div class="uk-form-row advanced-fiter <?php echo (!$showAdvansedFiler) ? ' uk-hidden' : ''; ?>">
-				<div class="uk-flex uk-flex-wrap uk-flex-space-between uk-flex-middle">
-					<div class="">
-						<?php /*
-						<div class="uk-flex-inline uk-margin-right uk-margin-top">
-							<?php $allregions = $this->filterForm->getField('allregions', 'filter'); ?>
-							<label for="<?php echo $allregions->id; ?>">
-								<?php echo $this->filterForm->getInput('allregions', 'filter'); ?>
-								<?php echo Text::_($allregions->label); ?>
-							</label>
-						</div>
-  						*/ ?>
-						<?php if (!Factory::getUser()->guest):
-							$onlymy = $this->filterForm->getField('onlymy', 'filter');
-							?>
-							<div class="uk-flex-inline uk-flex-middle uk-margin-top">
-								<label for="<?php echo $onlymy->id; ?>">
-									<?php echo $this->filterForm->getInput('onlymy', 'filter'); ?>
-									<?php echo Text::_('COM_BOARD_ONLYMY_ITEMS'); ?>
-								</label>
-							</div>
-						<?php endif; ?>
-					</div>
-					<div class="uk-flex-inline uk-margin-top">
-						<div class="uk-button-group">
-							<a href="<?php echo $this->link; ?>" class="uk-button uk-button-danger">
-								<?php echo Text::_('JCLEAR'); ?>
-							</a>
-							<button type="submit" class="uk-button uk-button-primary">
-								<?php echo Text::_('JSEARCH_FILTER_SUBMIT'); ?>
-							</button>
-						</div>
-					</div>
-				</div>
-			</div>
 		</form>
 	</div>
 
 	<?php if ($this->items) : ?>
-		<div class="items">
+		<div class="items uk-panel uk-panel-box uk-margin-bottom">
 			<?php
 			$count = count($this->items);
 			$half  = round($count / 2);
@@ -128,7 +68,7 @@ HTMLHelper::_('script', 'modalmap.min.js', array('version' => 'auto', 'relative'
 
 				<?php endif; ?>
 
-				<div class="item uk-panel uk-panel-box uk-margin-bottom">
+				<div class="item">
 					<div class="title uk-flex uk-flex-space-between">
 						<?php $item->author_link = $item->link;
 						echo LayoutHelper::render('content.author.horizontal', $item); ?>
@@ -147,108 +87,54 @@ HTMLHelper::_('script', 'modalmap.min.js', array('version' => 'auto', 'relative'
 							</div>
 						</div>
 					</div>
-					<a class="uk-margin-top uk-grid uk-grid-small uk-link-muted" href="<?php echo $item->link; ?>">
-						<div class="uk-width-small-3-4">
-							<h2 class="uk-h4 uk-margin-small-bottom">
-								<?php echo $item->title; ?>
+					<a class="uk-margin-top-remove uk-grid uk-grid-small uk-link-muted" href="<?php echo $item->link; ?>">
+						<div class="uk-width-small-1-1">
+							<div class="uk-margin-small-bottom">
 								<?php if ($item->for_when == 'today'): ?>
-									<sup class="uk-badge uk-badge-success uk-margin-small-left">
+									<span class="uk-badge uk-badge-success uk-margin-small-right">
 										<?php echo Text::_('COM_BOARD_ITEM_FOR_WHEN_TODAY'); ?>
-									</sup>
+									</span>
 								<?php elseif ($item->for_when == 'tomorrow'): ?>
-									<sup class="uk-badge uk-badge-notification uk-margin-small-left">
+									<span class="uk-badge uk-badge-notification uk-margin-small-right">
 										<?php echo Text::_('COM_BOARD_ITEM_FOR_WHEN_TOMORROW'); ?>
-									</sup>
+									</span>
 								<?php endif; ?>
 								<?php if (!$item->state): ?>
-									<sup class="uk-badge uk-badge-warning uk-margin-small-left">
+									<span class="uk-badge uk-badge-warning uk-margin-small-right">
 										<?php echo Text::_('TPL_NERUDAS_ONMODERATION'); ?>
-									</sup>
+									</span>
 								<?php endif; ?>
 								<?php if ($item->publish_down !== '0000-00-00 00:00:00' &&
 									$item->publish_down < Factory::getDate()->toSql()): ?>
-									<sup class="uk-badge uk-badge-danger uk-margin-small-left">
+									<span class="uk-badge uk-badge-danger uk-margin-small-right">
 										<?php echo Text::_('TPL_NERUDAS_PUBLISH_TIMEOUT'); ?>
-									</sup>
+									</span>
 								<?php endif; ?>
-							</h2>
+							</div>
 							<div>
 								<?php
 								$text = JHtmlString::truncate($item->text, 100, false, false);
 								$text = str_replace('...', '', $text);
 								?>
 								<span class="uk-text-small"><?php echo !empty($text) ? $text . '... ' : ''; ?></span>
-								<span class="uk-link"><?php echo Text::_('TPL_NERUDAS_READMORE'); ?></span>
 							</div>
 						</div>
-						<div class="uk-width-small-1-4 uk-flex uk-flex-top uk-flex-right">
-							<?php if (!empty($item->price) ||
-								$item->payment_method == 'cashless' || $item->payment_method == 'cash' ||
-								$item->prepayment == 'required' || $item->prepayment == 'no') : ?>
-								<div class="uk-price uk-text-right">
-									<?php if (!empty($item->price)) : ?>
-										<div class="text">
-											<?php echo ($item->price == '-0') ? Text::_('JGLOBAL_FIELD_PRICE_CONTRACT_PRICE') :
-												$item->price . ' ' . Text::_('JGLOBAL_FIELD_PRICE_CURRENCY_RUB'); ?>
-										</div>
+					</a>
+					<div class="uk-margin-small-top uk-grid uk-grid-small">
+						<div class="uk-width-small-2-3 uk-flex uk-flex-bottom">
+							<?php if (!empty($item->tags->itemTags)): ?>
+								<div class="tags">
+									<?php if ($item->tags): ?>
+										<?php foreach ($item->tags->itemTags as $tag): ?>
+											<span class="uk-tag">
+												<?php echo $tag->title; ?>
+											</span>
+										<?php endforeach; ?>
 									<?php endif; ?>
-									<div class="uk-text-right">
-										<?php if ($item->payment_method == 'cashless')
-										{
-											echo HTMLHelper::image('icons/payment_method_cashless.png',
-												Text::_('COM_BOARD_ITEM_PAYMENT_METHOD_CASHLESS'),
-												array('title'           => Text::_('COM_BOARD_ITEM_PAYMENT_METHOD_CASHLESS'),
-												      'data-uk-tooltip' => ''),
-												true);
-
-										}
-										elseif ($item->payment_method == 'cash')
-										{
-											echo HTMLHelper::image('icons/payment_method_cash.png',
-												Text::_('COM_BOARD_ITEM_PAYMENT_METHOD_CASH'),
-												array('title'           => Text::_('COM_BOARD_ITEM_PAYMENT_METHOD_CASH'),
-												      'data-uk-tooltip' => ''),
-												true);
-										}
-
-										if ($item->prepayment == 'required')
-										{
-											echo HTMLHelper::image('icons/prepayment_required.png',
-												Text::_('COM_BOARD_ITEM_PREPAYMENT_REQUIRED'),
-												array('title'           => Text::_('COM_BOARD_ITEM_PREPAYMENT_REQUIRED'),
-												      'data-uk-tooltip' => ''),
-												true);
-
-										}
-										elseif ($item->prepayment == 'no')
-										{
-											echo HTMLHelper::image('icons/prepayment_no.png',
-												Text::_('COM_BOARD_ITEM_PREPAYMENT_NO'),
-												array('title'           => Text::_('COM_BOARD_ITEM_PREPAYMENT_NO'),
-												      'data-uk-tooltip' => ''),
-												true);
-										} ?>
-									</div>
 								</div>
 							<?php endif; ?>
 						</div>
-					</a>
-					<div class="uk-margin-top uk-grid uk-grid-small">
-						<div class="uk-width-small-2-3 uk-flex uk-flex-bottom">
-							<?php if ($item->contacts->get('phones', false)) : ?>
-								<?php foreach ($item->contacts->get('phones') as $phone): ?>
-									<a class="uk-text-xlarge uk-display-block"
-									   href="tel:<?php echo $phone->code . $phone->number; ?>">
-										<?php $phone->display = (!empty($phone->display)) ?
-											$phone->display : $phone->code . $phone->number;
-										$regular              = "/(\\+\\d{1})(\\d{3})(\\d{3})(\\d{2})(\\d{2})/";
-										$subst                = '$1($2)$3-$4-$5';
-										echo preg_replace($regular, $subst, $phone->display); ?>
-									</a>
-									<?php break; endforeach; ?>
-							<?php endif; ?>
-						</div>
-						<div class="uk-width-small-1-3 uk-flex uk-flex-right uk-flex-bottom">
+						<div class="uk-width-small-1-3 uk-flex uk-flex-right uk-flex-top">
 							<?php
 							$item->region = ($item->region == '*') ? 100 : $item->region;
 							echo HTMLHelper::image('regions/' . $item->region . '.png', $item->region_name,
@@ -267,42 +153,14 @@ HTMLHelper::_('script', 'modalmap.min.js', array('version' => 'auto', 'relative'
 							<?php endif; ?>
 						</div>
 					</div>
-					<?php if ($item->image) : ?>
-						<div class="uk-margin-top">
-							<div class="uk-grid uk-grid-small image">
-								<?php
-								$count = count($item->images);
-								foreach ($item->images as $image): ?>
-									<div class="uk-container-center
-									<?php echo 'uk-width-small-1-' . $count; ?>">
-										<a class="uk-position-relative uk-display-block"
-										   href="<?php echo $item->link; ?>">
-											<span class="image uk-thumbnail uk-display-block uk-cover-background"
-												  style="background-image: url('<?php echo $image['src']; ?>')"
-												  data-ratio-height="[166,125]"></span>
-										</a>
-									</div>
-								<?php endforeach; ?>
-							</div>
-						</div>
-					<?php endif; ?>
-					<?php if (!empty($item->tags->itemTags)): ?>
-						<div class="uk-margin-small-top tags">
-							<?php if ($item->tags): ?>
-								<?php foreach ($item->tags->itemTags as $tag): ?>
-									<span class="uk-tag">
-										<?php echo $tag->title; ?>
-									</span>
-								<?php endforeach; ?>
-							<?php endif; ?>
-						</div>
-					<?php endif; ?>
 				</div>
+				<hr>
 				<?php $i++; endforeach; ?>
+			<div>
+				<?php echo $this->pagination->getPagesLinks(); ?>
+			</div>
 		</div>
-		<div>
-			<?php echo $this->pagination->getPagesLinks(); ?>
-		</div>
+
 	<?php endif; ?>
 </div>
 
--- html/mod_board_latest/home.php
+++ html/mod_board_latest/home.php
@@ -30,10 +30,9 @@ HTMLHelper::_('script', 'modalmap.min.js', array('version' => 'auto', 'relative'
 		</a>
 	</div>
 	<?php if ($items) : ?>
-		<div class="items">
+		<div class="items uk-panel uk-panel-box uk-margin-bottom">
 			<?php foreach ($items as $id => $item): ?>
-
-				<div class="item uk-panel uk-panel-box uk-margin-bottom">
+				<div class="item">
 					<div class="title uk-flex uk-flex-space-between">
 						<?php $item->author_link = $item->link;
 						echo LayoutHelper::render('content.author.horizontal', $item); ?>
@@ -53,108 +52,53 @@ HTMLHelper::_('script', 'modalmap.min.js', array('version' => 'auto', 'relative'
 							</div>
 						</div>
 					</div>
-					<a class="uk-margin-top uk-grid uk-grid-small uk-link-muted" href="<?php echo $item->link; ?>">
-						<div class="uk-width-small-3-4">
-							<h2 class="uk-h4 uk-margin-small-bottom">
-								<?php echo $item->title; ?>
-								<?php if ($item->for_when == 'today'): ?>
-									<sup class="uk-badge uk-badge-success uk-margin-small-left">
-										<?php echo Text::_('COM_BOARD_ITEM_FOR_WHEN_TODAY'); ?>
-									</sup>
-								<?php elseif ($item->for_when == 'tomorrow'): ?>
-									<sup class="uk-badge uk-badge-notification uk-margin-small-left">
-										<?php echo Text::_('COM_BOARD_ITEM_FOR_WHEN_TOMORROW'); ?>
-									</sup>
-								<?php endif; ?>
-								<?php if (!$item->state): ?>
-									<sup class="uk-badge uk-badge-warning uk-margin-small-left">
-										<?php echo Text::_('TPL_NERUDAS_ONMODERATION'); ?>
-									</sup>
-								<?php endif; ?>
-								<?php if ($item->publish_down !== '0000-00-00 00:00:00' &&
-									$item->publish_down < Factory::getDate()->toSql()): ?>
-									<sup class="uk-badge uk-badge-danger uk-margin-small-left">
-										<?php echo Text::_('TPL_NERUDAS_PUBLISH_TIMEOUT'); ?>
-									</sup>
-								<?php endif; ?>
-							</h2>
-							<div>
-								<?php
-								$text = JHtmlString::truncate($item->text, 100, false, false);
-								$text = str_replace('...', '', $text);
-								?>
-								<span class="uk-text-small"><?php echo !empty($text) ? $text . '... ' : ''; ?></span>
-								<span class="uk-link"><?php echo Text::_('TPL_NERUDAS_READMORE'); ?></span>
-							</div>
+					<a class="uk-margin-top-remove uk-link-muted" href="<?php echo $item->link; ?>">
+						<div class="uk-margin-small-bottom">
+							<?php if ($item->for_when == 'today'): ?>
+								<span class="uk-badge uk-badge-success uk-margin-small-right">
+									<?php echo Text::_('COM_BOARD_ITEM_FOR_WHEN_TODAY'); ?>
+								</span>
+							<?php elseif ($item->for_when == 'tomorrow'): ?>
+								<span class="uk-badge uk-badge-notification uk-margin-small-right">
+									<?php echo Text::_('COM_BOARD_ITEM_FOR_WHEN_TOMORROW'); ?>
+								</span>
+							<?php endif; ?>
+							<?php if (!$item->state): ?>
+								<span class="uk-badge uk-badge-warning uk-margin-small-right">
+									<?php echo Text::_('TPL_NERUDAS_ONMODERATION'); ?>
+								</span>
+							<?php endif; ?>
+							<?php if ($item->publish_down !== '0000-00-00 00:00:00' &&
+								$item->publish_down < Factory::getDate()->toSql()): ?>
+								<span class="uk-badge uk-badge-danger uk-margin-small-right">
+									<?php echo Text::_('TPL_NERUDAS_PUBLISH_TIMEOUT'); ?>
+								</span>
+							<?php endif; ?>
 						</div>
-						<div class="uk-width-small-1-4 uk-flex uk-flex-top uk-flex-right">
-							<?php if (!empty($item->price) ||
-								$item->payment_method == 'cashless' || $item->payment_method == 'cash' ||
-								$item->prepayment == 'required' || $item->prepayment == 'no') : ?>
-								<div class="uk-price uk-text-right">
-									<?php if (!empty($item->price)) : ?>
-										<div class="text">
-											<?php echo ($item->price == '-0') ? Text::_('JGLOBAL_FIELD_PRICE_CONTRACT_PRICE') :
-												$item->price . ' ' . Text::_('JGLOBAL_FIELD_PRICE_CURRENCY_RUB'); ?>
-										</div>
+						<div>
+							<?php
+							$text = JHtmlString::truncate($item->text, 100, false, false);
+							$text = str_replace('...', '', $text);
+							?>
+							<span class="uk-text-small"><?php echo !empty($text) ? $text . '... ' : ''; ?></span>
+						</div>
+
+					</a>
+					<div class="uk-margin-small-top uk-grid uk-grid-small">
+						<div class="uk-width-small-2-3 uk-flex uk-flex-bottom">
+							<?php if (!empty($item->tags->itemTags)): ?>
+								<div class="uk-margin-small-top tags">
+									<?php if ($item->tags): ?>
+										<?php foreach ($item->tags->itemTags as $tag): ?>
+											<span class="uk-tag">
+												<?php echo $tag->title; ?>
+											</span>
+										<?php endforeach; ?>
 									<?php endif; ?>
-									<div class="uk-text-right">
-										<?php if ($item->payment_method == 'cashless')
-										{
-											echo HTMLHelper::image('icons/payment_method_cashless.png',
-												Text::_('COM_BOARD_ITEM_PAYMENT_METHOD_CASHLESS'),
-												array('title'           => Text::_('COM_BOARD_ITEM_PAYMENT_METHOD_CASHLESS'),
-												      'data-uk-tooltip' => ''),
-												true);
-
-										}
-										elseif ($item->payment_method == 'cash')
-										{
-											echo HTMLHelper::image('icons/payment_method_cash.png',
-												Text::_('COM_BOARD_ITEM_PAYMENT_METHOD_CASH'),
-												array('title'           => Text::_('COM_BOARD_ITEM_PAYMENT_METHOD_CASH'),
-												      'data-uk-tooltip' => ''),
-												true);
-										}
-
-										if ($item->prepayment == 'required')
-										{
-											echo HTMLHelper::image('icons/prepayment_required.png',
-												Text::_('COM_BOARD_ITEM_PREPAYMENT_REQUIRED'),
-												array('title'           => Text::_('COM_BOARD_ITEM_PREPAYMENT_REQUIRED'),
-												      'data-uk-tooltip' => ''),
-												true);
-
-										}
-										elseif ($item->prepayment == 'no')
-										{
-											echo HTMLHelper::image('icons/prepayment_no.png',
-												Text::_('COM_BOARD_ITEM_PREPAYMENT_NO'),
-												array('title'           => Text::_('COM_BOARD_ITEM_PREPAYMENT_NO'),
-												      'data-uk-tooltip' => ''),
-												true);
-										} ?>
-									</div>
 								</div>
 							<?php endif; ?>
 						</div>
-					</a>
-					<div class="uk-margin-top uk-grid uk-grid-small">
-						<div class="uk-width-small-2-3 uk-flex uk-flex-bottom">
-							<?php if ($item->contacts->get('phones', false)) : ?>
-								<?php foreach ($item->contacts->get('phones') as $phone): ?>
-									<a class="uk-text-xlarge uk-display-block"
-									   href="tel:<?php echo $phone->code . $phone->number; ?>">
-										<?php $phone->display = (!empty($phone->display)) ?
-											$phone->display : $phone->code . $phone->number;
-										$regular              = "/(\\+\\d{1})(\\d{3})(\\d{3})(\\d{2})(\\d{2})/";
-										$subst                = '$1($2)$3-$4-$5';
-										echo preg_replace($regular, $subst, $phone->display); ?>
-									</a>
-									<?php break; endforeach; ?>
-							<?php endif; ?>
-						</div>
-						<div class="uk-width-small-1-3 uk-flex uk-flex-right uk-flex-bottom">
+						<div class="uk-width-small-1-3 uk-flex uk-flex-right uk-flex-top">
 							<?php
 							$item->region = ($item->region == '*') ? 100 : $item->region;
 							echo HTMLHelper::image('regions/' . $item->region . '.png', $item->region_name,
@@ -173,37 +117,8 @@ HTMLHelper::_('script', 'modalmap.min.js', array('version' => 'auto', 'relative'
 							<?php endif; ?>
 						</div>
 					</div>
-					<?php if ($item->image) : ?>
-						<div class="uk-margin-top">
-							<div class="uk-grid uk-grid-small image">
-								<?php
-								$count = count($item->images);
-								foreach ($item->images as $image): ?>
-									<div class="uk-container-center
-									<?php echo 'uk-width-small-1-' . $count; ?>">
-										<a class="uk-position-relative uk-display-block"
-										   href="<?php echo $item->link; ?>">
-											<span class="image uk-thumbnail uk-display-block uk-cover-background"
-												  style="background-image: url('<?php echo $image['src']; ?>')"
-												  data-ratio-height="[166,125]"></span>
-										</a>
-									</div>
-								<?php endforeach; ?>
-							</div>
-						</div>
-					<?php endif; ?>
-					<?php if (!empty($item->tags->itemTags)): ?>
-						<div class="uk-margin-small-top tags">
-							<?php if ($item->tags): ?>
-								<?php foreach ($item->tags->itemTags as $tag): ?>
-									<span class="uk-tag">
-										<?php echo $tag->title; ?>
-									</span>
-								<?php endforeach; ?>
-							<?php endif; ?>
-						</div>
-					<?php endif; ?>
 				</div>
+				<hr>
 			<?php endforeach; ?>
 			<div class="more uk-text-center">
 				<a href="<?php echo $categoryLink; ?>"
