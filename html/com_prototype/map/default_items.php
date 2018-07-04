ae1453069ac6b4a0622f5add4fec6dfa3ec20612
Septdir <septdir@gmail.com>
2018-07-04 16:42:02 +0300

    Change list

--- html/com_prototype/map/default_items.php
+++ html/com_prototype/map/default_items.php
@@ -17,85 +17,75 @@ use Joomla\Registry\Registry;
 ?>
 <?php if ($this->items): ?>
 	<?php foreach ($this->items as $item):
-		$catFields = new Registry($item->category->get('fields')); ?>
+		$text = '';
+		if (!empty($item->extra->get('why_you')))
+		{
+			$text = $item->extra->get('why_you');
+		}
+		if (!empty($item->extra->get('comment')))
+		{
+			$text = $item->extra->get('comment');
+		}
+		$text = JHtmlString::truncate($text, 50, false, false);
+
+		$catFields = new Registry($item->category->get('fields'));
+
+		$price_type = false;
+		if ($catFields->get('price_m3') && $catFields->get('price_t'))
+		{
+			$price_type = $this->extra_filter->get('price_m3t', 'm3');
+		}
+		if ($catFields->get('price_h') && $catFields->get('price_s'))
+		{
+			$price_type = $this->extra_filter->get('price_hs', 'h');
+		}
+		if ($catFields->get('price_o'))
+		{
+			$price_type = 'o';
+		}
+
+		$onModeration = (!$item->state || ($item->publish_down !== '0000-00-00 00:00:00' && $item->publish_down < Factory::getDate()->toSql()));
+		?>
 		<div class="item" data-show="false" data-prototype-item="<?php echo $item->id; ?>">
 			<a class="uk-link-muted uk-display-block  uk-padding"
 			   data-prototype-show="<?php echo $item->id; ?>">
-				<div class="uk-text-medium">
-					<?php echo $item->title; ?>
+				<div class="title uk-grid uk-grid-small">
+					<div class="uk-width-medium-<?php echo ($price_type) ? '3-5' : '1-1'; ?>">
+						<div class=" uk-text-medium">
+							<?php echo $item->title; ?>
+						</div>
+						<div class="uk-text-small uk-text-muted">
+							<?php if ($item->category->get('parent_level') > 1): ?>
+								<span><?php echo $item->category->get('parent_title'); ?> </span>
+							<?php endif; ?>
+							<span><?php echo $item->category->get('title'); ?></span>
+						</div>
+					</div>
+					<?php if ($price_type): ?>
+						<div class="uk-width-medium-2-5 uk-text-right">
+							<div class="price uk-margin-small-top">
+								<div>
+									<span class="uk-text-bold">
+										<?php echo $item->extra->get('price_' . $price_type, '---'); ?>
+									</span>
+									<span class="uk-text-mini uk-text-muted uk-text-uppercase">
+										<?php echo Text::_('TPL_NERUDAS_PRICE_TYPE_RUB')
+											. '/' . Text::_('TPL_NERUDAS_PRICE_TYPE_' . $price_type); ?>
+									</span>
+								</div>
+							</div>
+						</div>
+					<?php endif; ?>
 				</div>
-				<?php if (!$item->state || ($item->publish_down !== '0000-00-00 00:00:00' &&
-						$item->publish_down < Factory::getDate()->toSql())): ?>
-					<div>
+
+				<div class="text uk-text-small uk-margin-small-top">
+					<?php echo $text; ?>
+				</div>
+				<?php if ($onModeration) : ?>
+					<div class="uk-margin-small-top">
 						<span class="uk-badge uk-badge-danger">
 							<?php echo Text::_('TPL_NERUDAS_ONMODERATION'); ?>
 						</span>
-					</div>
-				<?php endif; ?>
-				<div class="uk-text-small uk-margin-small-bottom">
-					<?php if ($item->category->get('parent_id') > 1): ?>
-						<span><?php echo $item->category->get('parent_title'); ?></span>
-						<span> / </span>
-					<?php endif; ?>
-					<span><?php echo $item->category->get('title'); ?></span>
-				</div>
-				<?php if ($catFields->get('price_m3') && $catFields->get('price_t')): ?>
-					<?php if ($this->extra_filter->get('price_m3t')):
-						$price_type = $this->extra_filter->get('price_m3t');
-						?>
-						<div class="uk-text-bold">
-							<?php echo $item->extra->get('price_' . $price_type, '---') . ' ' . Text::_('JGLOBAL_FIELD_PRICE_CURRENCY_RUB'); ?>
-						</div>
-					<?php else: ?>
-						<div class="uk-text-bold">
-							<span class="uk-margin-small-right">
-								<?php echo $item->extra->get('price_m3', '..') . ' ' .
-									Text::_('TPL_NERUDAS_PRICE_TYPE_RUB')
-									. '/' . Text::_('TPL_NERUDAS_PRICE_TYPE_M3'); ?>
-							</span>
-							<span class="uk-margin-small-right">
-								<?php echo $item->extra->get('price_t', '..') . ' ' .
-									Text::_('TPL_NERUDAS_PRICE_TYPE_RUB')
-									. '/' . Text::_('TPL_NERUDAS_PRICE_TYPE_T'); ?>
-							</span>
-						</div>
-					<?php endif; ?>
-				<?php endif; ?>
-				<?php if (!empty($item->extra->get('price_o'))): ?>
-					<div class="uk-text-bold">
-						<?php echo $item->extra->get('price_o') . ' ' . Text::_('JGLOBAL_FIELD_PRICE_CURRENCY_RUB'); ?>
-					</div>
-				<?php endif; ?>
-				<?php if ($catFields->get('price_h') && $catFields->get('price_s')): ?>
-					<?php if ($this->extra_filter->get('price_hs')):
-						$price_type = $this->extra_filter->get('price_hs');
-						?>
-						<div class="uk-text-bold">
-							<?php echo $item->extra->get('price_' . $price_type, '---') . ' ' . Text::_('JGLOBAL_FIELD_PRICE_CURRENCY_RUB'); ?>
-						</div>
-					<?php else: ?>
-						<div class="uk-text-bold">
-							<span class="uk-margin-small-right">
-								<?php echo $item->extra->get('price_h', '..') . ' ' .
-									Text::_('TPL_NERUDAS_PRICE_TYPE_RUB')
-									. '/' . Text::_('TPL_NERUDAS_PRICE_TYPE_H'); ?>
-							</span>
-							<span class="uk-margin-small-right">
-								<?php echo $item->extra->get('price_s', '..') . ' ' .
-									Text::_('TPL_NERUDAS_PRICE_TYPE_RUB')
-									. '/' . Text::_('TPL_NERUDAS_PRICE_TYPE_S'); ?>
-							</span>
-						</div>
-					<?php endif; ?>
-				<?php endif; ?>
-				<?php if (!empty($item->extra->get('why_you'))): ?>
-					<div class="uk-text-muted uk-text-small">
-						<?php echo JHtmlString::truncate($item->extra->get('why_you'), 50, false, false); ?>
-					</div>
-				<?php endif; ?>
-				<?php if (!empty($item->extra->get('comment'))): ?>
-					<div class="uk-text-muted uk-text-small">
-						<?php echo JHtmlString::truncate($item->extra->get('comment'), 50, false, false); ?>
 					</div>
 				<?php endif; ?>
 			</a>
