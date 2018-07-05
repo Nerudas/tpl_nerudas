2575566e45c864d97a95db7ebab53d5eea253636
Septdir <septdir@gmail.com>
2018-07-05 15:23:03 +0300

    Add profiles short codes

---
+++ html/layouts/components/com_profiles/shortcodes/discussions.php
@@ -0,0 +1,29 @@
+<?
+/**
+ * @package    Nerudas Template
+ * @version    4.9.20
+ * @author     Nerudas  - nerudas.ru
+ * @copyright  Copyright (c) 2013 - 2018 Nerudas. All rights reserved.
+ * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
+ * @link       https://nerudas.ru
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Language\Text;
+
+$item = $displayData;
+
+$text = (!empty($item->status)) ? $item->status :
+	JHtmlString::truncate($item->about, 250, false, false);
+
+?>
+
+<a href="<?php echo $item->link; ?>" class="uk-panel uk-display-block uk-panel-box uk-panel-box-secondary">
+	<div class="text uk-margin-small-bottom uk-text-muted">
+		<?php echo $text; ?>
+	</div>
+	<div class="uk-text-right uk-margin-small-top">
+		<span class="uk-button"><?php echo Text::_('TPL_NERUDAS_READMORE'); ?></span>
+	</div>
+</a>
---
+++ html/layouts/components/com_profiles/shortcodes/discussions_preview.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package    Nerudas Template
+ * @version    4.9.20
+ * @author     Nerudas  - nerudas.ru
+ * @copyright  Copyright (c) 2013 - 2018 Nerudas. All rights reserved.
+ * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
+ * @link       https://nerudas.ru
+ */
+
+defined('_JEXEC') or die;
+
+$item = $displayData;
+
+echo JHtmlString::truncate((!empty($item->status)) ? $item->status : $item->about, 70, false, false);
