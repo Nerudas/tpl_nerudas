f997c3e4c68b91125017705437e60e3592479161
Septdir <septdir@gmail.com>
2018-07-05 12:21:29 +0300

    Change version to 4.9.20

--- css/fonts.css
+++ css/fonts.css
@@ -1,6 +1,6 @@
 /*
  * @package    Nerudas Template
- * @version    4.9.19
+ * @version    4.9.20
  * @author     Nerudas  - nerudas.ru
  * @copyright  Copyright (c) 2013 - 2018 Nerudas. All rights reserved.
  * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
