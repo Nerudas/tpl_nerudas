c456c5a4fde717f576a996ee2772136d3a15d44e
Septdir <septdir@gmail.com>
2018-08-02 13:05:35 +0300

    Add location region override

---
+++ html/mod_location_regions/default_items.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @package    Nerudas Template
+ * @version    4.9.24
+ * @author     Nerudas  - nerudas.ru
+ * @copyright  Copyright (c) 2013 - 2018 Nerudas. All rights reserved.
+ * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
+ * @link       https://nerudas.ru
+ */
+
+defined('_JEXEC') or die;
+
+?>
+<div>
+	<?php foreach ($items as $item): ?>
+		<div>
+			<a data-location-set-region="<?php echo $item->id; ?>">
+				<?php echo ($item->level > 1) ? str_repeat('- ', ($item->level - 1)) . $item->name : $item->name; ?>
+			</a>
+		</div>
+	<?php endforeach; ?>
+</div>
