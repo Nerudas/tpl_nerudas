eda8879b51939b34f700a5f860373f4ef00d7b52
Septdir <septdir@gmail.com>
2018-09-26 14:36:07 +0300

    Override images field

---
+++ html/layouts/joomla/form/field/files/images.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @package    Nerudas Template
+ * @version    4.9.27
+ * @author     Nerudas  - nerudas.ru
+ * @copyright  Copyright (c) 2013 - 2018 Nerudas. All rights reserved.
+ * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
+ * @link       https://nerudas.ru
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+
+extract($displayData);
+
+HTMLHelper::_('jquery.framework');
+HTMLHelper::_('stylesheet', 'media/plg_fieldtypes_files/css/images.min.css', array('version' => 'auto'));
+HTMLHelper::_('script', 'media/plg_fieldtypes_files/js/images.js', array('version' => 'auto'));
+HTMLHelper::_('jquery.ui');
+HTMLHelper::_('jquery.ui', array('sortable'));
+?>
+<div id="<?php echo $id; ?>" class="<?php echo $class; ?>" data-input-images="<?php echo $id; ?>"
+	 data-name="<?php echo $name; ?>">
+	<div class="form">
+		<input id="<?php echo $id; ?>_field" class="file" type="file" accept="image/*" multiple/>
+		<div class="loading"></div>
+		<label for="<?php echo $id; ?>_field" class="uk-link-muted"></label>
+		<div class="text">
+			<div><i id="upload-icon" class="uk-icon-upload"></i></div>
+			<div class="button">
+				<span class="btn btn-success"><?php echo Text::_('JGLOBAL_FIELD_IMAGES_CHOOSE_BUTTON'); ?></span>
+			</div>
+		</div>
+	</div>
+	<div class="limit-error uk-alert uk-alert-danger">
+		<?php echo Text::sprintf('JGLOBAL_FIELD_IMAGES_LIMIT_ERROR', $limit); ?>
+	</div>
+	<div class="result"></div>
+</div>
---
+++ html/layouts/joomla/form/field/files/images/items.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @package    Nerudas Template
+ * @version    4.9.27
+ * @author     Nerudas  - nerudas.ru
+ * @copyright  Copyright (c) 2013 - 2018 Nerudas. All rights reserved.
+ * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
+ * @link       https://nerudas.ru
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Uri\Uri;
+
+jimport('joomla.filesystem.file');
+
+$images = $displayData;
+$root   = '/' . trim(Uri::root(true));
+?>
+<ul class="uk-position-relative">
+	<?php foreach ($images as $image): ?>
+		<li class="item<?php echo ($image->text !== false) ? ' with-text' : '' ?>"
+			data-key="<?php echo $image->file; ?>">
+			<div class="wrapper">
+				<a href="<?php echo $root . $image->src; ?>" target="_blank" class="icon">
+					<img src="<?php echo $root . $image->src; ?>">
+				</a>
+				<div class="text">
+					<?php if ($image->text === false) : ?>
+						<div class="uk-h4 uk-margin-small-bottom">
+							<?php echo JFile::getName($image->src); ?>
+						</div>
+					<?php endif; ?>
+					<div><code><?php echo $image->src; ?></code></div>
+					<?php if ($image->text !== false) : ?>
+						<div class="description">
+							<textarea class="uk-width-1-1" rows="3" name="<?php echo $image->filed_name; ?>[text]"
+							><?php echo $image->text; ?></textarea>
+						</div>
+					<?php endif; ?>
+				</div>
+			</div>
+
+			<div class="actions">
+				<a class="remove uk-text-danger uk-icon-remove" data-file="<?php echo $image->src; ?>"></a>
+				<a class="move uk-text-primary uk-icon-arrows"></a>
+			</div>
+			<input type="hidden" name="<?php echo $image->filed_name; ?>[ordering]"
+				   value="<?php echo $image->ordering; ?>">
+		</li>
+	<?php endforeach; ?>
+</ul>
