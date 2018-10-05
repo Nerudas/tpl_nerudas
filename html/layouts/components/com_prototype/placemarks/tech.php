655d6d809a3db1012d2ac39d06fb0b49ec4a19b4
Septdir <septdir@gmail.com>
2018-10-05 13:33:45 +0300

    Change version to 4.9.28

--- html/layouts/components/com_prototype/placemarks/tech.php
+++ html/layouts/components/com_prototype/placemarks/tech.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package    techas Template
- * @version    4.9.27
+ * @version    4.9.28
  * @author     techas  - techas.ru
  * @copyright  Copyright (c) 2013 - 2018 techas. All rights reserved.
  * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
