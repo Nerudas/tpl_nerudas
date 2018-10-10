f7dfcb2047d1b57cbdea0331076dd7ef653ff12c
Septdir <septdir@gmail.com>
2018-10-10 12:53:12 +0300

    Change version to 4.9.29

--- html/com_discussions/topics/default.php
+++ html/com_discussions/topics/default.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package    Nerudas Template
- * @version    4.9.28
+ * @version    4.9.29
  * @author     Nerudas  - nerudas.ru
  * @copyright  Copyright (c) 2013 - 2018 Nerudas. All rights reserved.
  * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
