90bf5e5bf8d9c660fa5b2d49aad3f6547bf209a3
Septdir <septdir@gmail.com>
2018-11-20 15:57:45 +0300

    Change version to 4.9.34

--- js/tabsmap.js
+++ js/tabsmap.js
@@ -1,6 +1,6 @@
 /*
  * @package    Nerudas Template
- * @version    4.9.33
+ * @version    4.9.34
  * @author     Nerudas  - nerudas.ru
  * @copyright  Copyright (c) 2013 - 2018 Nerudas. All rights reserved.
  * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
