3c38b254a91b5d6c76cd0660806930cf50f16496
Septdir <septdir@gmail.com>
2018-12-04 13:47:04 +0300

    Change version to 4.9.36

--- js/modalmap.js
+++ js/modalmap.js
@@ -1,6 +1,6 @@
 /*
  * @package    Nerudas Template
- * @version    4.9.35
+ * @version    4.9.36
  * @author     Nerudas  - nerudas.ru
  * @copyright  Copyright (c) 2013 - 2018 Nerudas. All rights reserved.
  * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
