f2f89ff653ad195321f37d5f969814f90898afb1
Septdir <septdir@gmail.com>
2019-01-24 15:49:07 +0300

    New proptypes list item

--- html/layouts/components/com_prototype/shortcodes/item/default.php
+++ html/layouts/components/com_prototype/shortcodes/item/default.php
@@ -30,32 +30,8 @@ extract($displayData);
 			<?php echo HTMLHelper::image($preset->get('icon'), $preset->get('title', $item->get('title'))); ?>
 		<?php endif; ?>
 	</div>
-	<div class="uk-width-medium-4-12">
-		<a class="uk-display-block uk-link-muted" data-prototype-shortcodes-show-balloon="<?php echo $item->get('id'); ?>">
-			<div class="uk-h3">
-				<?php echo $item->get('title'); ?>
-			</div>
-
-			<?php if (!$item->get('active')): ?>
-				<span class="uk-badge uk-badge-danger">
-					<?php echo Text::_('TPL_NERUDAS_PROTOTYPE_NOACTIVE'); ?>
-				</span>
-			<?php endif; ?>
-			<div class="uk-text-small uk-text-muted uk-text-lowercase uk-margin-small-bottom">
-				<?php if ($category->get('parent_level') > 1): ?>
-					<span><?php echo $category->get('parent_title'); ?> </span>
-				<?php endif; ?>
-				<span><?php echo $category->get('title'); ?></span>
-			</div>
-			<?php if (!empty($item->get('text'))): ?>
-				<div class="uk-text-small uk-text-muted">
-					<?php echo JHtmlString::truncate($item->get('text'), 70, false, false); ?>
-				</div>
-			<?php endif; ?>
-		</a>
-	</div>
 	<div class="uk-width-medium-1-12 uk-text-center">
-		<a class="uk-display-block uk-link-muted" data-prototype-shortcodes-show-balloon="<?php echo $item->get('id'); ?>">
+		<a class="uk-display-block uk-link-muted" data-prototype-list-show-balloon="<?php echo $item->get('id'); ?>">
 			<div class="uk-text-nowrap uk-text-bold uk-text-xlarge">
 				<?php if (!empty($item->get('price'))) : ?>
 					<?php echo ($item->get('price') > 0) ? $item->get('price') : '---'; ?> ₽
@@ -68,28 +44,67 @@ extract($displayData);
 			</div>
 		</a>
 	</div>
-	<div class="uk-width-medium-2-12 uk-text-center uk-text-muted">
-		<?php echo HTMLHelper::image('templates/nerudas/images/prototype-map-icon.png', ''); ?>
-		<?php if (!empty($preset->get('delivery'))) : ?>
-			<span> <?php echo $preset->get('delivery')->title; ?></span>
-		<?php endif; ?>
-		<?php if (!empty($item->get('location'))): ?>
-			<span> <?php echo $item->get('location'); ?></span>
+	<div class="uk-width-medium-2-12 uk-text-center">
+		<?php if (!empty($item->get('images'))): ?>
+			<a data-prototype-list-show-balloon="<?php echo $item->get('id'); ?>">
+				<?php echo HTMLHelper::image($item->get('image'), $preset->get('title', $item->get('title'))); ?>
+			</a>
 		<?php endif; ?>
 	</div>
-	<div class="uk-width-medium-3-12 uk-flex uk-flex-top">
-		<div>
-			<a class="author uk-link-muted uk-display-inline-block"
-			   data-prototype-list-show-author="<?php echo $item->get('id'); ?>">
-				<div class="name uk-link uk-text-nowrap">
-					<?php echo JHtmlString::truncate($author->get('name'), 25, true, false); ?>
-				</div>
-				<?php if (!empty($author->get('signature'))): ?>
-					<div class="job uk-text-uppercase-letter uk-text-small  uk-text-small uk-text-muted uk-text-ellipsis uk-text-nowrap">
-						[ <?php echo JHtmlString::truncate($author->get('signature'), 20, true, false); ?> ]
-					</div>
+	<div class="uk-width-medium-5-12">
+		<a class="uk-display-block uk-link-muted" data-prototype-list-show-balloon="<?php echo $item->get('id'); ?>">
+			<div class="uk-h3">
+				<?php echo $item->get('title'); ?>
+				<?php if (!$item->get('active')): ?>
+					<span class="uk-badge uk-badge-danger">
+						<?php echo Text::_('TPL_NERUDAS_PROTOTYPE_NOACTIVE'); ?>
+					</span>
 				<?php endif; ?>
-			</a>
+			</div>
+			<div class="uk-text-small uk-text-muted uk-text-lowercase uk-margin-small-bottom">
+				<?php if ($category->get('parent_level') > 1): ?>
+					<span><?php echo $category->get('parent_title'); ?> </span>
+				<?php endif; ?>
+				<span><?php echo $category->get('title'); ?></span>
+			</div>
+			<?php if (!empty($item->get('text'))): ?>
+				<div class="uk-text-small">
+					<?php echo JHtmlString::truncate($item->get('text'), 50, false, false); ?>
+				</div>
+			<?php endif; ?>
+			<p>
+				<?php echo HTMLHelper::image('templates/nerudas/images/prototype-map-icon.png', ''); ?>
+				<?php if (!empty($preset->get('delivery'))) : ?>
+					<span> <?php echo $preset->get('delivery')->title; ?></span>
+				<?php endif; ?>
+				<?php if (!empty($item->get('location'))): ?>
+					<span> <?php echo $item->get('location'); ?></span>
+				<?php endif; ?>
+			</p>
+		</a>
+	</div>
+	<div class="uk-width-medium-3-12 uk-flex uk-flex-top">
+
+		<div>
+			<div class="uk-margin-bottom">
+				<time class="timeago uk-text-muted uk-text-nowrap "
+					  data-uk-tooltip
+					  datetime="<?php echo HTMLHelper::date($item->get('date'), 'c'); ?>"
+					  title="<?php echo HTMLHelper::date($item->get('date'), 'd.m.Y H:i'); ?>"></time>
+			</div>
+			<div>
+				<a class="author uk-link-muted uk-display-inline-block"
+				   data-prototype-list-show-author="<?php echo $item->get('id'); ?>">
+					<div class="name uk-link uk-text-nowrap">
+						<?php echo JHtmlString::truncate($author->get('name'), 25, true, false); ?>
+					</div>
+					<?php if (!empty($author->get('signature'))): ?>
+						<div class="job uk-text-uppercase-letter uk-text-small  uk-text-small uk-text-muted uk-text-ellipsis uk-text-nowrap">
+							[ <?php echo JHtmlString::truncate($author->get('signature'), 20, true, false); ?> ]
+						</div>
+					<?php endif; ?>
+				</a>
+			</div>
 			<?php if ($author->get('contacts', false) && is_object($item->get('payment_down', false))
 				&& !$item->get('payment_down')->end) : ?>
 				<div>
@@ -116,11 +131,5 @@ extract($displayData);
 				</div>
 			<?php endif; ?>
 		</div>
-	</div>
-	<div class="uk-width-medium-1-12 uk-text-center uk-flex uk-flex-top uk-flex-center">
-		<a class="author uk-link-muted uk-display-inline-block"
-		   data-prototype-shortcodes-show-author="<?php echo $item->get('id'); ?>">
-			<?php echo HTMLHelper::image('templates/nerudas/images/prototype-call-icon.png', ''); ?>
-		</a>
 	</div>
 </div>
