ba9bf05cf1b1b1a20ef0ec719a4125aa048f1b4f
Septdir <septdir@gmail.com>
2019-01-29 20:17:10 +0300

    Change version to 4.9.42

--- html/layouts/components/com_info/shortcodes/discussions.php
+++ html/layouts/components/com_info/shortcodes/discussions.php
@@ -1,7 +1,7 @@
 <?
 /**
  * @package    Nerudas Template
- * @version    4.9.40
+ * @version    4.9.42
  * @author     Nerudas  - nerudas.ru
  * @copyright  Copyright (c) 2013 - 2018 Nerudas. All rights reserved.
  * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
--- html/layouts/components/com_prototype/list/item/mobile.php
+++ html/layouts/components/com_prototype/list/item/mobile.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package    Nerudas Template
- * @version    4.9.40
+ * @version    4.9.42
  * @author     Nerudas  - nerudas.ru
  * @copyright  Copyright (c) 2013 - 2019 Nerudas. All rights reserved.
  * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
